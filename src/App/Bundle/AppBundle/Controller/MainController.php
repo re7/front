ba0b5d1605fadfc6f84dab02d86107bc196328ab
Emeric Kasbarian <emerick42@gmail.com>
2014-12-04 21:10:23 +0100

    Add the ability to configure a piwik to track stats

--- src/App/Bundle/AppBundle/Controller/MainController.php
+++ src/App/Bundle/AppBundle/Controller/MainController.php
@@ -18,4 +18,20 @@ class MainController extends Controller
     {
         return $this->render(':App/Main:index.html.twig');
     }
+
+    /**
+     * The action to render the piwik tracking code
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function piwikAction()
+    {
+        $siteId     = $this->container->hasParameter('piwik.site_id') ? $this->container->getParameter('piwik.site_id') : null;
+        $trackerUrl = $this->container->hasParameter('piwik.tracker_url') ? $this->container->getParameter('piwik.tracker_url') : null;
+
+        return $this->render(':App:piwik.html.twig', [
+            'siteId'     => $siteId,
+            'trackerUrl' => $trackerUrl,
+        ]);
+    }
 }
